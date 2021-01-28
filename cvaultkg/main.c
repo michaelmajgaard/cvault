396afb3802ae2182362f727e715c37da552d5000
michaelmajgaard <michaelmajgaard@outlook.com>
2021-01-28 20:23:09 +0100

    cvaultkg bit sizes

--- cvaultkg/main.c
+++ cvaultkg/main.c
@@ -7,11 +7,10 @@
 
 void print_help() {
     printf("usage:\n"
-           "\tcvaultkg --size 256 --output <file>\n"
-           "\tcvaultkg -s 256 -o <file>\n\n"
+           "\tcvaultkg --size 256\n"
+           "\tcvaultkg -s 256 \n\n"
            "options:\n"
-           "\t-o --output <file>\tPath to key file.\n"
-           "\t-s --size {256 | 512}\tKey size in bits.\n");
+           "\t-s --size {64 | 128 | 256 | 512}\tKey size in bits.\n");
 }
 
 int parse_mut_excl_arg(char *arg, int optc, char *opts[]) {
@@ -23,38 +22,37 @@ int parse_mut_excl_arg(char *arg, int optc, char *opts[]) {
     return 0;
 }
 
-int parse_pos_args(char *argv[], int kfi, int si, char **kf, char **s) {
-    return (!strcmp(argv[kfi], "-o") || !strcmp(argv[kfi], "--output")) &&
-           (!strcmp(argv[si], "-s") || !strcmp(argv[si], "--size")) &&
-           (*kf = argv[kfi + 1]) && (*s = argv[si + 1]);
+int parse_pos_args(char *argv[], char **s) {
+    return (!strcmp(argv[1], "-s") || !strcmp(argv[1], "--size")) &&
+           (*s = argv[2]);
 }
 
-int parse_args(int argc, char *argv[], char **kf, char **s) {
-    if (argc == 5 && parse_pos_args(argv, 1, 3, kf, s) ||
-        parse_pos_args(argv, 3, 1, kf, s)) {
-        char *optv[3] = {"256", "512"};
-        return parse_mut_excl_arg(*s, 2, optv);
+int parse_args(int argc, char *argv[], char **s) {
+    if (argc == 3 && parse_pos_args(argv, s)) {
+        char *optv[4] = {"64", "128", "256", "512"};
+        return parse_mut_excl_arg(*s, 4, optv);
     }
     return 0;
 }
 
-char *genkey(int l) {
-    char *buffer = (char *)malloc((l + 1) * sizeof(char));
-    for (int i = 0; i < l; ++i) {
+char *genkey(int bit_size) {
+    int length = bit_size / 8;
+    char *buffer = (char *)malloc((length + 1) * sizeof(char));
+    for (int i = 0; i < length; ++i) {
         buffer[i] = 126 - rand() % 93;
     }
-    buffer[l] = '\0';
+    buffer[length] = '\0';
     return buffer;
 }
 
 int main(int argc, char *argv[]) {
-    char *kf, *si, *key;
-    if (!parse_args(argc, argv, &kf, &si)) {
+    char *si, *key;
+    if (!parse_args(argc, argv, &si)) {
         print_help();
         return -1;
     }
     srand(time(0));
-    key = genkey(32);
+    key = genkey(atoi(si));
     printf("%s\n", key);
     free(key);
     return 0;
